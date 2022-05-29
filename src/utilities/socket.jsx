44782e59f0b5df54b8f47fe79b1acdaccf627ec7
Ravindra Nag <ravindranag52@gmail.com>
2022-05-29 23:43:12 +0530

    iHope this works

--- src/utilities/socket.jsx
+++ src/utilities/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:8000')
+const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:8000')
 
 const broadcast = (msg) => {
     socket.emit('send', msg)
